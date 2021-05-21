b81c516d7a8955664855a9b63f7734b0c52c38dc
rishi <xmpuri@yahoo.com>
2021-05-20 20:29:06 -0700

    updates

--- deeplearning/image_utils.py
+++ deeplearning/image_utils.py
@@ -5,7 +5,7 @@ from builtins import range
 import urllib.request, urllib.error, urllib.parse, os, tempfile
 
 import numpy as np
-from scipy.misc.pilutil import imread, imresize
+from cv2 import imread, imresize
 
 """
 Utility functions used for viewing and processing images.
